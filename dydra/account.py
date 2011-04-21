e81facc6937e0f87d96d54612c414d2b1145f290
Arto Bendiken <arto@bendiken.net>
2011-04-22 00:01:17 +0200

    Implemented the Account#__iter__ method.

--- dydra/account.py
+++ dydra/account.py
@@ -13,4 +13,9 @@ class Account(dydra.Resource):
     return "dydra.Account('%s')" % (self.name)
 
   def __getitem__(self, key):
-    return dydra.Repository(self.name + '/' + str(key))
+    return dydra.Repository(self.name + '/' + str(key), client=self.client)
+
+  def __iter__(self):
+    repository_list = self.client.call('repository.list', self.name)
+    for (account_name, repository_name) in repository_list:
+      yield dydra.Repository(self.name + '/' + repository_name, client=self.client)
