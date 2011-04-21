b59e64bb552a4cd79c8a03ad4b8350fe6574391b
Arto Bendiken <arto@bendiken.net>
2011-04-21 22:10:58 +0200

    Implemented an XML-RPC API client stub.

--- dydra/__init__.py
+++ dydra/__init__.py
@@ -1,1 +1,7 @@
 __version__ = '0.0.0'
+
+HOST     = 'api.dydra.com'
+BASE_URL = 'http://' + HOST + '/'
+AUTH_URL = 'http://%s:%s@' + HOST + '/'
+
+from dydra.client import Client
---
+++ dydra/client.py
@@ -0,0 +1,22 @@
+import dydra
+import xmlrpclib # FIXME: use xmlrpc.client for Python 3.0+
+
+class Client(object):
+  """A client for the Dydra.com XML-RPC API."""
+
+  url = None
+  rpc = None
+
+  def __init__(self, **kwargs):
+    if kwargs.has_key('url'):
+      self.url = str(kwargs['url'])
+    elif kwargs.has_key('token'):
+      self.url = dydra.BASE_URL + 'rpc?auth_token=' + str(kwargs['token'])
+    elif kwargs.has_key('username') and kwargs.has_key('password'):
+      self.url = (dydra.AUTH_URL % (str(kwargs['username']), str(kwargs['password']))) + 'rpc'
+    else:
+      raise RuntimeError('no user credentials supplied')
+    self.rpc = xmlrpclib.ServerProxy(self.url)
+
+  def __repr__(self):
+    return "dydra.Client(url='%s')" % (self.url)
