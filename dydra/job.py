c0655b6e36e2cacd9b407a7791bcc721b8f0ed20
Arto Bendiken <arto@bendiken.net>
2011-05-10 21:45:08 +0200

    Added some inline API documentation for the Job class.

--- dydra/job.py
+++ dydra/job.py
@@ -8,43 +8,72 @@ from time import sleep
 class Job(dydra.Resource):
   """Represents a Dydra.com job."""
 
+  STATUS_UNKNOWN   = 'unknown'
+  STATUS_PENDING   = 'pending'
+  STATUS_RUNNING   = 'running'
+  STATUS_COMPLETED = 'completed'
+  STATUS_FAILED    = 'failed'
+  STATUS_ABORTED   = 'aborted'
+
   ##
   # (Attribute) The job UUID.
   uuid = None
 
   ##
+  # Initializes the job instance.
+  #
   # @param uuid A valid job UUID.
   def __init__(self, uuid, **kwargs):
-    self.uuid = str(uuid)
     super(Job, self).__init__(self.uuid, **kwargs) # FIXME
+    self.uuid = str(uuid)
 
   ##
+  # Returns a string representation of the job UUID.
+  #
   # @return A string representation of this object.
   def __repr__(self):
     return "dydra.Job('%s')" % (self.uuid)
 
+  ##
+  # Returns `True` if this job is currently pending to run.
   def is_pending(self):
-    return self.status() == 'pending'
+    return self.status() == STATUS_PENDING
 
+  ##
+  # Returns `True` if this job is currently running.
   def is_running(self):
-    return self.status() == 'running'
+    return self.status() == STATUS_RUNNING
 
+  ##
+  # Returns `True` if this job has already completed.
   def is_completed(self):
-    return self.status() == 'completed'
+    return self.status() == STATUS_COMPLETED
 
+  ##
+  # Returns `True` if this job failed for some reason.
   def is_failed(self):
-    return self.status() == 'failed'
+    return self.status() == STATUS_FAILED
 
+  ##
+  # Returns `True` if this job was aborted for any reason.
   def is_aborted(self):
-    return self.status() == 'aborted'
+    return self.status() == STATUS_ABORTED
 
+  ##
+  # Returns `True` if this job has completed or was aborted, and
+  # `False` if it's currently pending or running.
   def is_done(self):
     return self.client.call('job.done', self.uuid)
 
+  ##
+  # Returns the current status of this job.
   def status(self):
     return self.client.call('job.status', self.uuid)
 
+  ##
+  # Waits until this job is done.
   def wait(self, **kwargs):
+    # TODO: timeout support
     delay = 0.5 # seconds
     if kwargs.has_key('delay'):
       delay = float(kwargs['delay'])
@@ -52,6 +81,8 @@ class Job(dydra.Resource):
       sleep(delay)
     return self
 
+  ##
+  # Aborts this job if it is currently pending or running.
   def abort(self):
     self.client.call('job.abort', self.uuid)
     return self
